28536acb35001d3fbe6339bfbde533aff980b714
kennethudoakpan <kennethakanimo@gmail.com>
2024-10-29 19:00:13 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import TopFlights from './components/TopFlights';
+import WhyUs from './components/WhyUs';
+import CustomerTestimonials from './components/Testimonials';
+import PopularQuestions from './components/FAQ';
+import Footer from './components/Footer'; 
+import './Styles.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main">
+        <Hero />
+        <TopFlights />
+        <WhyUs />
+        <CustomerTestimonials />
+        <PopularQuestions />
+      </main>
+      <Footer />
+    </div>
+  );
+}
